a50c736a47613c769f8594b9c2af70d38a07d1e2
feven <fevenbayu5@gmail.com>
2024-03-24 11:38:15 -0700

    challenge

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,6 +1,6 @@
 ###
 #
-#  Sort integer arguments (ascending)
+#  Sort integer arguments (ascending) 
 #
 ###
 
@@ -11,38 +11,7 @@ ARGV.each do |arg|
 
     # convert to integer
     i_arg = arg.to_i
-
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i, i_arg)
-            is_inserted = true
-            break
-        end
-    end
-    result << i_arg if !is_inserted
-end
-
-puts result
-###
-#
-#  Sort integer arguments (ascending)
-#
-###
-
-result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
-
-    # convert to integer
-    i_arg = arg.to_i
-
+    
     # insert result at the right position
     is_inserted = false
     i = 0
